c53e5799d10b3b2747f0e288dd8b297a956f98fc
Matias Dominguez <matias.dominguez@cosmesoft.com.py>
2026-06-04 23:47:45 -0300

    locale y datezone movido a common

--- common/config-common.nix
+++ common/config-common.nix
@@ -6,6 +6,21 @@
     shell = pkgs.zsh;
   };
 
+  time.timeZone = "America/Asuncion";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_PY.UTF-8";
+    LC_IDENTIFICATION = "es_PY.UTF-8";
+    LC_MEASUREMENT = "es_PY.UTF-8";
+    LC_MONETARY = "es_PY.UTF-8";
+    LC_NAME = "es_PY.UTF-8";
+    LC_NUMERIC = "es_PY.UTF-8";
+    LC_PAPER = "es_PY.UTF-8";
+    LC_TELEPHONE = "es_PY.UTF-8";
+    LC_TIME = "es_PY.UTF-8";
+  };
+
   environment.variables.COLORTERM = "truecolor";
 
   programs = {
