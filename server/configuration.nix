3e5907800815a4ffed00246df927c72e53af7d6b
Matias Dominguez <matias.dominguez@cosmesoft.com.py>
2026-06-04 23:46:20 -0300

    locale y datezone movido a common

--- server/configuration.nix
+++ server/configuration.nix
@@ -16,22 +16,6 @@
 
   networking.hostName = "nix-mach";
 
-  time.timeZone = "America/Asuncion";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_PY.UTF-8";
-    LC_IDENTIFICATION = "es_PY.UTF-8";
-    LC_MEASUREMENT = "es_PY.UTF-8";
-    LC_MONETARY = "es_PY.UTF-8";
-    LC_NAME = "es_PY.UTF-8";
-    LC_NUMERIC = "es_PY.UTF-8";
-    LC_PAPER = "es_PY.UTF-8";
-    LC_TELEPHONE = "es_PY.UTF-8";
-    LC_TIME = "es_PY.UTF-8";
-  };
-
   users.users."matias" = {
     extraGroups = [ "wheel" "docker" ];
   };
